1d2e8ac8ebc2c44b9a8961d23f30582e3d62a3da
Miftakhussolikhin <39107062+mftlhn@users.noreply.github.com>
2021-03-06 07:55:28 +0700

    update-content

--- js/script.js
+++ js/script.js
@@ -12,11 +12,47 @@ $(window).scroll(function() {
 
     if(scroll >= 200) {
         navbar.addClass('shadow');
-        navbar.addClass('bg-light');
+        navbar.addClass('bg-grey');
         navbar.removeClass('bg-transparent');
     } else {
         navbar.removeClass('shadow');
-        navbar.removeClass('bg-light');
+        navbar.removeClass('bg-grey');
         navbar.addClass('bg-transparent');
     }
 })
+
+$('#selengkapnya').mouseover(function() {
+    $('#selengkapnya').addClass('shadow');
+})
+
+$('#selengkapnya').mouseleave(function() {
+    $('#selengkapnya').removeClass('shadow');
+})
+
+AOS.init();
+
+$.getJSON("https://data.covid19.go.id/public/api/update.json", function(result) {
+    $('#odp').html(`
+        <h2>` + formatter(result.data.jumlah_odp) + `</h2>
+    `);
+    $('#spesimen').html(`
+        <h2>` + formatter(result.data.total_spesimen) + `</h2>
+    `);
+})
+
+function formatter(angka) {
+    var bilangan = angka;
+	
+    var	number_string = bilangan.toString(),
+        sisa 	= number_string.length % 3,
+        rupiah 	= number_string.substr(0, sisa),
+        ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+            
+    if (ribuan) {
+        separator = sisa ? '.' : '';
+        rupiah += separator + ribuan.join('.');
+    }
+
+    return rupiah;
+}
+
